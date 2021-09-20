2d48cc0a2d8a8b29c6fa77ca1c7fdb85aa09fcea
yaseen <mohammedbyasin@gmail.com>
2021-09-20 07:49:14 +0530

    updated date of website footer as dynamic

--- Intermediate/day56/server.py
+++ Intermediate/day56/server.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    current_year = datetime.now().year
+    return render_template('index.html', year=current_year)
 
 
 if __name__ == "__main__":
